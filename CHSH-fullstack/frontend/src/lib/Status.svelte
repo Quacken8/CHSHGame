9ebe924252eea4659ee6abaf4386d3cc3366af83
Ondra Janoška <o.janoska98@gmail.com>
2023-03-07 21:59:23 +0100

    heart indicator c:

--- CHSH-fullstack/frontend/src/lib/Status.svelte
+++ CHSH-fullstack/frontend/src/lib/Status.svelte
@@ -1,8 +1,20 @@
 <script lang="ts">
+    import Fa from "svelte-fa"
+    import {
+        faHeart,
+        faHeartCrack
+    } from "@fortawesome/free-solid-svg-icons";
+    export let connected = true;
 </script>
 
 <div>
-    <p>Status OK (don't trust me)</p>
+    <p>Stav připojení: 
+        {#if connected}
+        OK <Fa icon={faHeart}/>
+        {:else}
+        ?? <Fa icon={faHeartCrack}/>
+        {/if}
+    </p>
 </div>
 
 <style>
