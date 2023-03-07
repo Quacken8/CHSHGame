1881dae3ac1b4c472d9f391d8a0a01b3794f6fc3
JindraZPrahy <jindra@immerda.ch>
2023-03-07 21:18:22 +0100

    Footer added

---
+++ CHSH-fullstack/frontend/src/lib/Footer.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+	import Status from './Status.svelte';
+	import LeaveSession from './LeaveSession.svelte';
+</script>
+
+
+<div class="footer">
+    <div class="footer-item">
+        <p>Alice|Bob</p>
+    </div>
+	<div class="footer-item">
+		<Status />
+	</div>
+	<div class="footer-item">
+		<LeaveSession />
+	</div>
+</div>
+
+<style>
+	.footer {
+		display: flex;
+        padding-bottom: 0vh;
+	}
+	.footer-item {
+		margin: 5px;
+        
+	}
+</style>
--- CHSH-fullstack/frontend/src/lib/GameModeSelect.svelte
+++ CHSH-fullstack/frontend/src/lib/GameModeSelect.svelte
@@ -1,27 +1,12 @@
 <script lang="ts">
-	import Status from './Status.svelte';
-	import LeaveSession from './LeaveSession.svelte';
+	import Footer from './Footer.svelte';
 </script>
 
 <div class="centering">
-<p>Ahoj</p>
+	<p>Ahoj</p>
 </div>
 
-<div class="footer">
-	<div class="footer-item">
-		<Status />
-	</div>
-	<div class="footer-item">
-		<LeaveSession />
-	</div>
-</div>
+<Footer />
 
 <style>
-	.footer {
-		display: flex;
-	}
-	.footer-item {
-		margin: 5px;
-        
-	}
 </style>
