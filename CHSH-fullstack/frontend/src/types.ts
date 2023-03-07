bb3aa0599b46eea44439d41bbcb751480e9f8f4f
Michal Grňo <m93a.cz@gmail.com>
2023-03-07 22:54:23 +0100

    něco

--- CHSH-fullstack/frontend/src/types.ts
+++ CHSH-fullstack/frontend/src/types.ts
@@ -1,27 +1,25 @@
-import { getContext, setContext } from "svelte";
-import { writable, type Writable } from "svelte/store";
-import { Connection } from "./connection";
+import { getContext, setContext } from 'svelte';
+import { writable, type Writable } from 'svelte/store';
+import { Connection } from './connection';
 
-
-const WS_SERVER_URL = "wss://quantum-be.dsrod.cz/qbit";
+const WS_SERVER_URL = 'wss://quantum-be.dsrod.cz/qbit';
 
 export interface AppState {
-  page: "lobby" | "gameModeSelect" | "measurement" | "results";
-  role: "server" | "client";
-  connection: Connection;
+	page: 'lobby' | 'gameModeSelect' | 'measurement' | 'results';
+	role: 'server' | 'client';
+	connection: Connection;
 }
 
 export const createAppState = (): Writable<AppState> => {
-  const s = writable<AppState>({
-    page: "gameModeSelect",
-    role: "client",
-    connection: new Connection(),
-  });
-  setContext('app-state', s);
-  return s;
-}
+	const s = writable<AppState>({
+		page: 'gameModeSelect',
+		role: 'client',
+		connection: new Connection()
+	});
+	setContext('app-state', s);
+	return s;
+};
 
 export const getAppState = (): Writable<AppState> | undefined => {
-  return getContext('app-state');
-}
-
+	return getContext('app-state');
+};
