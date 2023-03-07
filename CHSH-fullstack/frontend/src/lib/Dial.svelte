5b925b311ef0fc2fa8de566e6bb50b4af1e28363
JindraZPrahy <dusekjindra@gmail.com>
2023-03-07 16:54:03 +0100

    Dial working

---
+++ CHSH-fullstack/frontend/src/lib/Dial.svelte
@@ -0,0 +1,113 @@
+<script lang="ts">
+let angleString: string = "0";
+$: angleNum = +angleString;
+$: angleOffset = angleNum + 180;
+$: angleDeg = String(angleOffset) + "deg";
+</script>
+
+<style>
+
+:root {
+    --angle: 180deg;
+    --size: 400px;
+    --clock-offset: 20px
+}
+
+.outer {
+    position: relative;
+    display: inline-block;
+    height: var(--size);
+    width:  var(--size);
+    background: lightgray;
+    border-radius: 50%;
+}
+
+
+.needle {
+    height: 100%;
+    width: 100%;
+    position: absolute;
+    top: 0;
+    left: 0;
+    border-radius: 50%;
+}
+
+.needle:before {
+    content: "";
+    position: absolute;
+    left: calc(50% - 2.5px);
+    top: 50%;
+    height: 48%;
+    width: 5px;
+    background: black;
+}
+
+.needle:after {
+    content: "";
+    position: absolute;
+    top: calc(98% + 5px);
+    left: 50%;
+    height: 10px;
+    width: 10px;
+    transform: rotate(-135deg);
+    transform-origin: top left;
+    border-top: 5px solid black;
+    border-left: 5px solid black;
+}
+
+.outer .needle {
+    transform: rotate(var(--angle));
+    transform-origin: center center;
+}
+
+
+
+
+.points {
+	position: absolute;
+    z-index: 2;
+	width: 10px;
+	height: 10px;
+	background: #777;
+	left: calc((var(--size)/2) - 5px   );
+	top: var(--clock-offset);
+	border-radius: 100%;
+	-webkit-transform-origin: 50% calc(var(--size)/2 - var(--clock-offset));
+	transform-origin: 50% calc(var(--size)/2 - var(--clock-offset));
+}
+.point-0 { -webkit-transform: rotate(0deg); transform: rotate(0deg); }
+.point-1 { -webkit-transform: rotate(30deg); transform: rotate(30deg); }
+.point-2 { -webkit-transform: rotate(60deg); transform: rotate(60deg); }
+.point-3 { -webkit-transform: rotate(90deg); transform: rotate(90deg); }
+.point-4 { -webkit-transform: rotate(120deg); transform: rotate(120deg); }
+.point-5 { -webkit-transform: rotate(150deg); transform: rotate(150deg); }
+.point-6 { -webkit-transform: rotate(180deg); transform: rotate(180deg); }
+.point-7 { -webkit-transform: rotate(210deg); transform: rotate(210deg); }
+.point-8 { -webkit-transform: rotate(240deg); transform: rotate(240deg); }
+.point-9 { -webkit-transform: rotate(270deg); transform: rotate(270deg); }
+.point-10 { -webkit-transform: rotate(300deg); transform: rotate(300deg); }
+.point-11 { -webkit-transform: rotate(330deg); transform: rotate(330deg); }
+
+
+</style>
+
+
+<div class="outer">
+    <div class="points point-0"  on:keydown={() => {}} on:click={() => console.log("a", (angleString = "00")) }></div>
+	<div class="points point-1"  on:keydown={() => {}} on:click={() => console.log("a", (angleString = "30")) }></div>
+	<div class="points point-2 " on:keydown={() => {}} on:click={() => console.log("a", (angleString = "60")) }></div>
+	<div class="points point-3 " on:keydown={() => {}} on:click={() => console.log("a", (angleString = "90")) }></div>
+	<div class="points point-4 " on:keydown={() => {}} on:click={() => console.log("a", (angleString = "120")) }></div>
+	<div class="points point-5 " on:keydown={() => {}} on:click={() => console.log("a", (angleString = "150")) }></div>
+	<div class="points point-6 " on:keydown={() => {}} on:click={() => console.log("a", (angleString = "180")) }></div>
+	<div class="points point-7 " on:keydown={() => {}} on:click={() => console.log("a", (angleString = "210")) }></div>
+	<div class="points point-8 " on:keydown={() => {}} on:click={() => console.log("a", (angleString = "240")) }></div>
+	<div class="points point-9 " on:keydown={() => {}} on:click={() => console.log("a", (angleString = "270")) }></div>
+	<div class="points point-10" on:keydown={() => {}} on:click={() => console.log("a", (angleString = "300")) }></div>
+	<div class="points point-11" on:keydown={() => {}} on:click={() => console.log("a", (angleString = "330")) }></div>
+    <div class="needle" style="--angle: {angleDeg}" ></div>
+</div>
+
+<br>
+
+<input bind:value={angleString}>
