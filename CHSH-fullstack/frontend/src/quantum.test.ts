e1765d8a5b2daee3b6dfd58d57a531dc6b193c20
Michal Grňo <m93a.cz@gmail.com>
2023-03-08 22:56:04 +0100

    we test

---
+++ CHSH-fullstack/frontend/src/quantum.test.ts
@@ -0,0 +1,14 @@
+import { describe, it, expect } from 'vitest';
+import { FourVector } from './quantum';
+
+describe('four-vector', () => {
+	it('can be instantiated', () => {
+		const v = new FourVector(1, 0, 2, 3);
+	});
+
+	it('does inner product', () => {
+		const a = new FourVector(1, 0, 2, 3);
+		const b = new FourVector(2, 3, -0.5, 0);
+		expect(a.dot(b)).equals(1);
+	});
+});
