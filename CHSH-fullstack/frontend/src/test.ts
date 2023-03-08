6b323911f162cced0f11ef54a0eb8cde78a71c85
Ondra Janoška <o.janoska98@gmail.com>
2023-03-08 18:24:48 +0100

    tests yay

---
+++ CHSH-fullstack/frontend/src/test.ts
@@ -0,0 +1,37 @@
+import { FourVector, EntangledQuBits } from "./quantum";
+
+var numberOfTests = 5000;
+
+var correctGuesses = 0.0;
+
+for (let i = 0; i < numberOfTests; i++) {
+    // create entangled pair
+    var initialState = FourVector(); // help what do i put here
+    var Qubits = new EntangledQuBits(initialState);
+
+    // recieve bits from charlie
+
+    var AliceBit = Math.floor(Math.random() * 2) // a floor of number between 0 and 1.99 is just a coin toss
+    var BobBit = Math.floor(Math.random() * 2)
+
+    // measure according to whateverthefuck i dont understand the entanglement
+
+    var angleOfMeasurementInDegs = 0; // ???? idk?
+    
+    var AliceMeasurementOutcome = Qubits.measureOneQuBit("Alice", angleOfMeasurementInDegs)
+    var BobMeasurementOutcome = Qubits.measureOneQuBit("Alice", angleOfMeasurementInDegs)
+
+    var AlicesSmartAndEntanglementInformedResponse = AliceMeasurementOutcome;
+    var BobSmartAndEntanglementInformedResponse = BobMeasurementOutcome;
+    
+    var didTheyDidTheThingProperly = (condition) ? true : false
+    if (didTheyDidTheThingProperly) {
+        correctGuesses += 1.0;
+    }
+}
+console.log("Correct guesses:");
+console.log(correctGuesses);
+console.log("Out of a total of:");
+console.log(numberOfTests);
+console.log("That is in percents:");
+console.log(correctGuesses / numberOfTests * 100); // oh boy i sure do hope there isnt anything funky regarding floats and ints here
