17cdf87118760679b7920115fd0be23242656b3f
Ondra Janoška <o.janoska98@gmail.com>
2023-03-09 14:47:54 +0100

    just making sure you get led to the right page after clicking start 1 or 100 games

--- CHSH-fullstack/frontend/src/lib/GameModeSelect.svelte
+++ CHSH-fullstack/frontend/src/lib/GameModeSelect.svelte
@@ -6,20 +6,19 @@
 	let appState = getAppState();
     
     $: gameState = $appState!.connection.data;
-
-	$: if ( $gameState.gameMode ===  'one-game' || $gameState.gameMode === 'many-games') {
-		appState!.update((s) => ({ ...s, page: 'measurement' }));
-	}
 	
 	const oneGame = (): void => {
 		console.log('Starting one game...');
 		appState!.update((s) => ({ ...s, gameMode: 'one-game'}));
 		gameState!.update((s) => ({ ...s, gameNumber: 1}));
+		appState!.update((s) => ({ ...s, page: 'measurement' }));
 	};
 	const hundredGames = (): void => {
 		console.log('Starting 100 games...');
 		$appState!.connection.data.value = {gameMode: 'many-games'};
 		gameState!.update((s) => ({ ...s, gameNumber: 1}));
+		gameState!.update((s) => ({ ...s, gamesWon: 0}));
+		appState!.update((s) => ({ ...s, page: 'measurement' }));
 	};
 </script>
 
